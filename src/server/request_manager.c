f0362389b05cc9964c3baf9709f74945a9799047
Ilias Tsatiris <eliastsatiris@gmail.com>
2018-05-19 19:08:52 +0300

    Fix bug with invalid writes

--- src/server/request_manager.c
+++ src/server/request_manager.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <time.h>
 
 #include "server_types.h"
@@ -107,15 +108,15 @@ void write_response(int fd, HttpError err, HttpRequest *request, char *full_path
     if (err == OK) 
         write_ok_response(fd, full_path);
     else
-        write_err_response(fd, BAD_REQUEST);
+        write_err_response(fd, err);
 }
 
 static
-HttpError check_file_access(char *file, char *root_dir, char **full_path) {
-    char *expanded_path = realpath(file, NULL);
+HttpError check_file_access(char *file, char *root_dir, char *full_path) {
+    P_DEBUG("File : (%s) Root : (%s)\n", file, root_dir);
+    char *expanded_path = realpath(file, full_path);
 
     if (expanded_path == NULL) {
-        P_DEBUG("File w/ root : (%s) Root dir : (%s)\n", file, root_dir);
         switch (errno) {
             case ENOENT:
             case ENOTDIR:
@@ -126,8 +127,6 @@ HttpError check_file_access(char *file, char *root_dir, char **full_path) {
                 return UNEXPECTED;
         }
     }
-
-    *full_path = expanded_path;
 
     struct stat f_stats;
 
@@ -170,7 +169,7 @@ void accept_http(void *arg) {
     char *root_dir = ((AcceptArgs*)arg)->root_dir;
     char *header   = NULL;
 
-    char *file_full_path = NULL;
+    char file_full_path[PATH_MAX + 1];
     char *file_w_root    = NULL;
 
     P_DEBUG("Got fd %d\n", fd);
@@ -209,16 +208,16 @@ void accept_http(void *arg) {
     memcpy(file_w_root, root_dir, root_len);
 
     // Copy file
-    memcpy(file_w_root + root_len, request->requested_file, root_len);
+    memcpy(file_w_root + root_len, request->requested_file, file_len);
 
-    if ((err = check_file_access(file_w_root, root_dir, &file_full_path)) != OK)
+
+    if ((err = check_file_access(file_w_root, root_dir, file_full_path)) != OK)
         goto CLOSE_CONNECTION;
-
 
 CLOSE_CONNECTION:
     write_response(fd, err, request, file_full_path);
     free_request(request);
     free(file_w_root);
-    free(file_full_path);
+    //free(file_full_path);
     close(fd);
 }
